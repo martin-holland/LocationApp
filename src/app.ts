45126808393e280e08804e74f2156319960f2c09
s2100688 <89017210+s2100688@users.noreply.github.com>
2022-02-18 13:55:00 +0200

    API call working

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
+import axios from "axios";
 
+console.log('Location app starts...');
+
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+const GOOGLE_API_KEY = "AIzaSyAb8LDRqUOhRoxYmH7Oop1F-aIMVKAT4io"
+
+function searchAddressHandler(event: Event) {
+    event.preventDefault();
+    const enteredAddress = addressInput.value
+
+   axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`).then((response) => {
+       console.log(response)
+   }).catch((error) => {
+       console.log(`Error: ${error}`)
+   })
+}
+
+form.addEventListener('submit', searchAddressHandler)
