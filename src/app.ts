619e3224b72c6238f7e05fe7729e41e0ab39dbe3
s2100688 <89017210+s2100688@users.noreply.github.com>
2022-02-18 14:16:25 +0200

    map working

--- src/app.ts
+++ src/app.ts
@@ -6,14 +6,32 @@ const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
 const GOOGLE_API_KEY = "AIzaSyAb8LDRqUOhRoxYmH7Oop1F-aIMVKAT4io"
+declare let google: any;
+
+// custom type for Google GeoCoding
+type GoogleGeoCodingResponse = {
+    results: {geometry: {location: {lat: number, lng: number}}}[];
+    status: 'OK' | 'ZERO_RESULTS'
+}
 
 function searchAddressHandler(event: Event) {
     event.preventDefault();
     const enteredAddress = addressInput.value
 
-   axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`).then((response) => {
-       console.log(response)
+   axios.get<GoogleGeoCodingResponse>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`).then((response) => {
+    if (response.data.status !== 'OK') {
+        throw new Error('Could not find location');
+    }
+    //    console.log(response)
+    const coordinates = response.data.results[0].geometry.location;
+    const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+        center: coordinates,
+        zoom: 8,
+      }
+      );
+      new google.maps.Marker({position: coordinates, map:map})
    }).catch((error) => {
+       alert(error.message)
        console.log(`Error: ${error}`)
    })
 }
